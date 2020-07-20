ae3aa734f420221ea205355e1e01bfab2681c038
Tomasz Bryndzia <tomasz.bryndzia@infopark.de>
2020-07-20 16:12:20 +0200

    delete editor notification

--- src/config/scrivitoMenu.js
+++ src/config/scrivitoMenu.js
@@ -1,17 +1,7 @@
 import * as Scrivito from "scrivito";
 
-Scrivito.extendMenu(menu => {
+Scrivito.extendMenu((menu) => {
   menu.remove("system.createPage");
-  menu.insert({
-    id: "editorNotification",
-    title: "Not sure how to name it",
-    icon: "/important-sign.svg", // TODO: icon is needed here
-    description: "Wichtige Nachrichten für den Redakteur.",
-    position: { after: "system.enableTour" }, // TODO: Clearify with customer
-    group: "system.help",
-    onClick: () => Scrivito.openDialog("EditorNotification"),
-    enabled: true,
-  });
   menu.insert({
     id: "webMigration",
     title: "Migrate content",
